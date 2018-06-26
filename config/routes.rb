410631ffad0bec59ea0c357479af063bcec9d10d
momentspace <momentspace@gmail.com>
2018-06-26 18:13:53 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'root#index'
+  root to: 'roots#index'
 
-  get  '/auth/:provider/callback' => 'omniauth_callbacks#callback'
-  post '/auth/:provider/callback'  => 'omniauth_callbacks#callback'
-  get  '/auth/failure' => 'omniauth_callbacks#failure'
-  get  '/logout' => 'omniauth_callbacks#destroy'
+  get  '/auth/:provider/callback' => 'omni_auth_callbacks#callback'
+  post '/auth/:provider/callback'  => 'omni_auth_callbacks#callback'
+  get  '/auth/failure' => 'omni_auth_callbacks#failure'
+  get  '/logout' => 'omni_auth_callbacks#destroy'
 end
